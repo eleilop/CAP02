0bdfd58be8f05469779381069a971c0aff588bb0
Eloy Leiva López <118524344+eleilop@users.noreply.github.com>
2023-10-08 19:37:57 +0200

    Add files via upload

---
+++ EJ06/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double base = 50.63;
+        double total = 121*base/100;
+        System.out.println("El total (base + IVA) es de "+total+" euros");
+    }
+}
---
+++ EJ07/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        char letra1 = 'H';
+        char letra2 = 'o';
+        char letra3 = 'l';
+        char letra4 = 'a';
+        String palabra = ", buenas tardes";
+
+        System.out.println(""+letra1+letra2+letra3+letra4+palabra);
+    }
+}
